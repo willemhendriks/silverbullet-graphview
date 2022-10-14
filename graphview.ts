71ae2163a81a7e8e2ea6204f8293af6cef6b58d3
Zef Hemel <zef@zef.me>
2022-10-14 16:06:09 +0200

    Upgrade to Deno-based build

--- graphview.ts
+++ graphview.ts
@@ -1,34 +1,36 @@
-import * as clientStore from "@silverbulletmd/plugos-silverbullet-syscall/clientStore";
-import {getCurrentPage, hideLhs, showLhs} from "@silverbulletmd/plugos-silverbullet-syscall/editor";
-import { listPages } from "@silverbulletmd/plugos-silverbullet-syscall/space";
-import { queryPrefix } from "@silverbulletmd/plugos-silverbullet-syscall";
+import {
+  clientStore,
+  editor,
+  index,
+  space,
+} from "$sb/silverbullet-syscall/mod.ts";
+import { asset } from "$sb/plugos-syscall/mod.ts";
 
-// @ts-ignore
-import d3js from "./lib/d3.js.txt"
-// @ts-ignore
-import d3forcejs from "./lib/d3-force.js.txt"
-// @ts-ignore
-import d3forcegraph from "./src/force-graph.js.txt"
-
-const GraphViewKey = "showGraphView"
+const GraphViewKey = "showGraphView";
 
 export async function toggleGraphView() {
   const showingGraphView = (await getGraphViewStatus());
   await clientStore.set(GraphViewKey, !showingGraphView);
   if (!showingGraphView) {
-    const name = await getCurrentPage(); 
+    const name = await editor.getCurrentPage();
     await renderGraph(name);
   } else {
-    await hideLhs();
+    await editor.hidePanel("lhs");
   }
 }
 
 // Use store to figure out if backlinks are open or closed.
-async function getGraphViewStatus() : Promise<boolean> {
+async function getGraphViewStatus(): Promise<boolean> {
   return !!(await clientStore.get(GraphViewKey));
 }
 
-function script(graph) {
+async function script(graph: any) {
+  const d3js = await asset.readAsset("asset/d3.js", "utf8");
+  const d3forcejs = await asset.readAsset("asset/d3-force.js", "utf8");
+  const d3forcegraph = await asset.readAsset(
+    "asset/force-graph.js",
+    "utf8",
+  );
   return `
     ${d3js}
     ${d3forcejs}
@@ -45,16 +47,18 @@ function script(graph) {
     });
     const graph_div = document.querySelector('#graph');
     graph_div.appendChild(chart);
-  `
+  `;
 }
 
 // render function into the LHS
-async function renderGraph(page) {
+async function renderGraph(page: any) {
   // https://github.com/d3/d3-force
   const graph = await buildGraph(page);
   const graph_json = JSON.stringify(graph);
   if (await getGraphViewStatus()) {
-    await showLhs(
+    await editor.showPanel(
+      "lhs",
+      1,
       `<html>
         <head>
         </head>
@@ -64,22 +68,23 @@ async function renderGraph(page) {
           </div>
         </body>
       </html>`,
-      script(graph_json), // Script (java script as string)
-      0.5
+      await script(graph_json), // Script (java script as string)
     );
   }
 }
 
-async function buildGraph(name) {
-  const pages = await listPages();
-  const nodeNames = pages.map(({name}) => { return name; });
+async function buildGraph(name: string) {
+  const pages = await space.listPages();
+  const nodeNames = pages.map(({ name }) => {
+    return name;
+  });
 
   // NOTE: This may result to the same link showing multiple times
   //       if the same page has multiple references to another page.
-  const pageLinks = await queryPrefix(`pl:`);
-  const links =  pageLinks.map(({key, page}) => { 
-    const [,to, ] = key.split(':'); // Key: pl:page:pos
-    
+  const pageLinks = await index.queryPrefix(`pl:`);
+  const links = pageLinks.map(({ key, page }) => {
+    const [, to] = key.split(":"); // Key: pl:page:pos
+
     if (!nodeNames.includes(to)) {
       // Add nodes for non-existing pages which are linked to
       nodeNames.push(to);
@@ -88,7 +93,7 @@ async function buildGraph(name) {
   });
 
   const nodes = nodeNames.map((name) => {
-    return {"id":name}
+    return { "id": name };
   });
 
   return { "nodes": nodes, "links": links };
